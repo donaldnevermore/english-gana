5e2d6d5e3c8bbffdccd2b63877c4f7e3943ee2e9
Donald33 Wang <donald33wang@gmail.com>
2024-08-28 00:09:22 +0800

    Update tokenize tests

--- src/english_gana/test_tokenize.py
+++ src/english_gana/test_tokenize.py
@@ -6,13 +6,13 @@ from english_gana.tokenize import remove_slash_pitch, tokenize_ipa
 def test_perfect():
     a = "/ˈpɜːrfɪkt/"
     s = tokenize_ipa(a)
-    assert "".join(s) == remove_slash_pitch(a)
+    assert s == ["p", "ɜː"] + list("rfɪkt")
 
 
 def test_boy():
     a = "/bɔɪ/"
     s = tokenize_ipa(a)
-    assert "".join(s) == remove_slash_pitch(a)
+    assert s == ["b", "ɔɪ"]
 
 
 def test_egg():
@@ -24,4 +24,4 @@ def test_egg():
 def test_house():
     a = "/haʊs/"
     s = tokenize_ipa(a)
-    assert "".join(s) == remove_slash_pitch(a)
+    assert s == ["h", "aʊ", "s"]
