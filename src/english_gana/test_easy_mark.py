469f52c94b11fa79d4a133d6d139bd3ccd06f78f
dcwong <donald33wang@gmail.com>
2025-05-31 07:10:45 +0000

    Update easy_mark

--- src/english_gana/test_easy_mark.py
+++ src/english_gana/test_easy_mark.py
@@ -5,5 +5,39 @@ from .tokenize import tokenize_ipa
 def test_easy_mark():
     txt = "/ˈpɜːrfɪkt/"
     s = tokenize_ipa(txt)
-    i = translate_to_easy_mark(s)
-    assert "".join(i) == "pe_rfikt"
+    x = translate_to_easy_mark(s)
+    assert "".join(x) == "piirfikt"
+
+def test_apple():
+    txt = "/ˈæpl/"
+    s = tokenize_ipa(txt)
+    x = translate_to_easy_mark(s)
+    assert "".join(x) == "aapl"
+
+
+def test_boy():
+    txt = "/bɔɪ/"
+    s = tokenize_ipa(txt)
+    x = translate_to_easy_mark(s)
+    assert "".join(x) == "boi"
+
+
+def test_egg():
+    txt = "/eɡ/"
+    s = tokenize_ipa(txt)
+    x = translate_to_easy_mark(s)
+    assert "".join(x) == "eg"
+
+
+def test_girl():
+    txt = "/ɡɜːrl/"
+    s = tokenize_ipa(txt)
+    x = translate_to_easy_mark(s)
+    assert "".join(x) == "giirl"
+
+
+def test_house():
+    txt = "/haʊs/"
+    s = tokenize_ipa(txt)
+    x = translate_to_easy_mark(s)
+    assert "".join(x) == "haus"
